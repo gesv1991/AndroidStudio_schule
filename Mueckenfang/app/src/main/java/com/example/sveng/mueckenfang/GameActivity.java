adfde03b130eb76612fb5d6bba9a59efa261cfa9
Sven Gebert <sveng1991@gmail.com>
2018-01-12 15:59:13 +0100

    update

--- Mueckenfang/app/src/main/java/com/example/sveng/mueckenfang/GameActivity.java
+++ Mueckenfang/app/src/main/java/com/example/sveng/mueckenfang/GameActivity.java
@@ -8,9 +8,41 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class GameActivity extends AppCompatActivity {
+    private boolean spielLaeuft;
+    private int runde;
+    private int punkte;
+    private int muecken;
+    private int gefangeneMuecken;
+    private int zeit;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
     }
+
+    // Ein neues Spiel starten
+    // Zweck: .........................
+    private void spielStarten() {
+        boolean spielLaeuft = true;
+        runde = 0;		// ...
+        punkte = 0;		// ...
+        starteRunde();
+    }
+
+    // Eine Runde starten
+    // Zweck: .........................
+    private void starteRunde()
+    {
+        runde = runde + 1;		// ...
+        muecken = runde * 10;	// ...
+        gefangeneMuecken = 0;	// ...
+        zeit = 60;	// ...
+        bildschirmAktualisieren();
+    }
+
+    private void bildschirmAktualisieren()
+    {
+        // wird später programmiert
+    }
+
 }
