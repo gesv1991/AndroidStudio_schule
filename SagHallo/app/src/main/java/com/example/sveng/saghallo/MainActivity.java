00ea86c296f2445ff777ff2d76ebb15cedc3cc09
Sven Gebert <sveng1991@gmail.com>
2018-01-12 09:20:15 +0100

    1

--- SagHallo/app/src/main/java/com/example/sveng/saghallo/MainActivity.java
+++ SagHallo/app/src/main/java/com/example/sveng/saghallo/MainActivity.java
@@ -2,23 +2,18 @@ package com.example.sveng.saghallo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.Locale;
-
-public class MainActivity extends Activity implements TextToSpeech.OnInitListener{
+public class MainActivity extends Activity implements TextToSpeech.OnInitListener, View.OnClickListener{
 
     private TextToSpeech tts;
     private RadioGroup radioLanguage;
-    private Button speak;
+    private Button speakButton;
     private RadioButton radioButton;
     private EditText editText;
     private String text;
@@ -28,38 +23,49 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tts = new TextToSpeech(this, this);
-
-        addListenerOnButton();
     }
+
     @Override
     public void onInit(int i) {
+        radioLanguage = (RadioGroup) findViewById(R.id.radioLanguage) ;
+        speakButton = (Button) findViewById(R.id.speakButton);
+
+        speakButton.setOnClickListener(this);
+
     }
 
-    public void addListenerOnButton() {
 
 
-        radioLanguage = (RadioGroup) findViewById(R.id.radioLanguage) ;
-        speak = (Button) findViewById(R.id.speakButton);
 
-        speak.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        EditText editText = (EditText)findViewById(R.id.speakText);
+        text = editText.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+}
+
+
+
+
+
+
+   /* public void addListenerOnButton() {
+        speakButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 int selectedId = radioLanguage.getCheckedRadioButtonId();
-                speak = (RadioButton) findViewById(selectedId);
-               text = editText.getText().toString();
-/*
-               if(speak.getText()=="Deutsch"){
+                speakButton = (RadioButton) findViewById(selectedId);
+
+            }
+        });
+    }
+
+               if(speakButton.getText()=="Deutsch"){
                     tts.setLanguage(Locale.GERMAN);
 
                 }
                 else{
                     tts.setLanguage(Locale.ENGLISH);
                 }*/
-                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-            }
-        });
-    }
-}
-
-
