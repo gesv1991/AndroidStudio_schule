8cd085b1dc4a1ab1f67c14c9c1486abbbc0be9d1
Sven Gebert <sveng1991@gmail.com>
2018-01-12 13:10:51 +0100

    fuck

---
+++ Mueckenfang/app/src/main/java/com/example/sveng/mueckenfang/Game.java
@@ -0,0 +1,16 @@
+package com.example.sveng.mueckenfang;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sveng on 12.01.2018.
+ */
+
+public class Game extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
---
+++ Mueckenfang/app/src/main/java/com/example/sveng/mueckenfang/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sveng.mueckenfang;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private Game game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+    public void onClick(View arg0) {
+
+        // Start NewActivity.class
+        Intent myIntent = new Intent(MainActivity.this,
+                Game.class);
+        startActivity(myIntent);
+    }
+});
+
+    }
+}
