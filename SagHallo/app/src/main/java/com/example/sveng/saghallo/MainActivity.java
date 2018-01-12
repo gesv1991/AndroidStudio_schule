bbe76760396e6b84263818f0a16bea5fc0064c98
Sven Gebert <sveng1991@gmail.com>
2018-01-12 10:04:19 +0100

    2

--- SagHallo/app/src/main/java/com/example/sveng/saghallo/MainActivity.java
+++ SagHallo/app/src/main/java/com/example/sveng/saghallo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sveng.saghallo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,8 +17,8 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     private TextToSpeech tts;
     private RadioGroup radioLanguage;
     private Button speakButton;
-    private RadioButton radioButton;
-    private EditText editText;
+    //   private RadioButton radioButton;
+    //   private EditText editText;
     private String text;
 
     @Override
@@ -31,13 +32,9 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
     public void onInit(int i) {
         radioLanguage = (RadioGroup) findViewById(R.id.radioLanguage) ;
         speakButton = (Button) findViewById(R.id.speakButton);
-
         speakButton.setOnClickListener(this);
 
     }
-
-
-
 
     @Override
     public void onClick(View view) {
@@ -46,13 +43,15 @@ public class MainActivity extends Activity implements TextToSpeech.OnInitListene
         speakButton = (RadioButton) findViewById(selectedId);
         EditText editText = (EditText)findViewById(R.id.speakText);
 
-        if(speakButton.getText()=="Deutsch"){
+        Log.i("what", speakButton);
+        if(speakButton.getText().toString()=="Deutsch"){
+            Log.i("deutsch", "Deutsch");
             tts.setLanguage(Locale.GERMAN);
             text = editText.getText().toString();
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-
         }
         else{
+            Log.i("english", "English Else");
             tts.setLanguage(Locale.ENGLISH);
             text = editText.getText().toString();
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
