2d302ff1722926e276a2ba3ff1ab82ebd7ba1bf4
Sven Gebert <30868569+gesv1991@users.noreply.github.com>
2018-01-22 22:29:56 +0100

    1

--- Ereigniss/app/src/main/java/com/example/sveng/ereigniss/MainActivity.java
+++ Ereigniss/app/src/main/java/com/example/sveng/ereigniss/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sveng.ereigniss;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
