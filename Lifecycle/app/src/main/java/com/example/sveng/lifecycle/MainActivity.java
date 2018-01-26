4d3560d71bdde9a17c361baebf7f530fe09f9e6e
Sven Gebert <sveng1991@gmail.com>
2018-01-26 12:00:40 +0100

    onStart

--- Lifecycle/app/src/main/java/com/example/sveng/lifecycle/MainActivity.java
+++ Lifecycle/app/src/main/java/com/example/sveng/lifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sveng.lifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Test: ", "oncreate");
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
